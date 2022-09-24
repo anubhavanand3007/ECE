9499e230738ae7b2fab5c28330f9bd8a1b28fbae
Anubhav Anand <88741077+anubhavanand3007@users.noreply.github.com>
2022-09-24 10:10:41 +0530

    Add files via upload

---
+++ exp7/exercise2.m
@@ -0,0 +1,1 @@
+clc, clear all, close all;wp = 0.2*pi;ws = 0.3*pi;N = ceil(6.6*pi/(ws-wp)); % estimates order of the filterk = kaiser(N+1,2.5);kb = fir1(N,[0.2 0.5],k);[H,W] = freqz(b,1,512);plot(W/pi,20*log10(abs(H))), grid on, title('FIR filter using Hamming window'); xlabel('Normalized Frequency'), ylabel('Gain in dB');
---
+++ exp7/sample1.m
@@ -0,0 +1,6 @@
+clc, clear all, close all;
+wp = 0.2*pi; ws = 0.3*pi;
+N = ceil(6.6*pi/(ws-wp)); % estimates order of the filter
+b = fir1(N, 0.2);
+[H,W] = freqz(b,1,512);
+plot(W/pi,20*log10(abs(H))), grid on, title('FIR filter using Hamming window'); xlabel('Normalized Frequency'), ylabel('Gain in dB');
