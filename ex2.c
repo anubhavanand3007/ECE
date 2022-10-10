3935e4c6e4c43c381ba649df57e0feadcb662b6d
Anubhav Anand <88741077+anubhavanand3007@users.noreply.github.com>
2022-10-10 16:51:19 +0530

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,20 +1,7 @@
-#include <lpc214x.h>
-int main(void){
-	unsigned long int c = 0xFF0000;
-	IO0DIR = c;
-	IO0SET = c;
-	while(1){
-		c = IO1PIN;
-		if(c == 0xffFE0000){
-			IO0PIN = c;
-		}else if(c == 0xffFD0000){
-			IO0PIN = 0XFC0000;
-		}else if(c == 0xffFB0000){
-			IO0PIN = 0XF00000;
-		}else if(c == 0xfff70000){
-			IO0PIN = 0X00;
-		}else{
-			IO0PIN = 0XFF0000;
-		}
-	}
+#include<lpc214x.h>
+
+void main(){
+		unsigned int a = 0x200;
+		IO0DIR = a;
+		IO0SET = a;
 }
